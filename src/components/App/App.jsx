1c2569b8a85bec02f44fc22552519cf9c75f6c88
NilsRondhuis <nilsrondhuis@ukr.net>
2022-12-02 17:17:00 +0200

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import Section from 'components/Section/Section';
+
+const App = () => {
+  return (
+    <>
+      <Section>
+        <p>Go coding</p>
+      </Section>
+    </>
+  );
+};
+
+export default App;
